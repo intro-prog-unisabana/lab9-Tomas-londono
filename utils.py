9b4452780800c0565d768190eaebfd3edfc5ba93
tomas londoño <tomaslonco@unisabana.edu.co>
2026-05-04 17:27:11 -0500

    9.8

--- utils.py
+++ utils.py
@@ -2,20 +2,16 @@ from person import Person
 from bank_account import BankAccount
 
 def person_data():
-    print("Enter the person's name:")
     name = input()
     person = Person(name)
 
     while True:
-        print("Enter a 4-digit account number:")
         acc_number = input()
-        print("Enter the initial balance:")
         balance = float(input())
 
         account = BankAccount(acc_number, balance)
         person.add_account(account)
 
-        print("Are you done adding accounts? (yes/no):")
         done = input()
         if done.lower() == "yes":
             break

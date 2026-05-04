0b5ed2f97397d646dafed214f1e7faa81ee3739e
tomas londoño <tomaslonco@unisabana.edu.co>
2026-05-04 17:40:52 -0500

    9.11

--- utils.py
+++ utils.py
@@ -2,16 +2,16 @@ from person import Person
 from bank_account import BankAccount
 
 def person_data():
-    name = input().strip()
+    name = input()
     person = Person(name)
 
     while True:
-        acc_number = input().strip()
-        balance = float(input().strip())
+        acc_number = input()
+        balance = float(input())
 
         person.add_account(BankAccount(acc_number, balance))
 
-        done = input().strip().lower()
+        done = input()
         if done == "yes":
             break
 
@@ -24,4 +24,4 @@ def balance_summary(person_list):
         for account in person.accounts:
             total_balance += account.balance
 
-        print(f"{person.name} : {total_balance:.2f}")# utils.py
+        print(f"{person.name} : {total_balance:.2f}")
